5c0442e04c0e987ea40ea0acd42372b38b078a1f
Jay8262-rgb <keshri425@gmail.com>
2025-09-07 13:38:58 +0530

    not page load functionality

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-   //upper part
-        link = "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=32977ceffadad0ca3ac83a080bfee99e"
-        var request = new XMLHttpRequest;
-        request.open('GET',link,true);
-        request.onload = function(){
-            var obj = JSON.parse(this.response);
-            console.log(obj);
-            document.getElementById('weather').innerHTML=obj.weather[0].description;
+function loadDelhi() {
+    console.log('Delhi function working');
+    var link = "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=32977ceffadad0ca3ac83a080bfee99e";
+    var request = new XMLHttpRequest();
+    request.open('GET', link, true);
+
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            let obj = JSON.parse(this.response);
+            document.getElementById('weather').innerHTML = obj.weather[0].description;
             document.getElementById('location').innerHTML = obj.name;
             document.getElementById('temp').innerHTML = Math.round(obj.main.temp - 273.15);
-            document.getElementById('icon').src = "https://openweathermap.org/img/w/" + obj.weather[0].icon + ".png"; 
+            document.getElementById('icon').src = "https://openweathermap.org/img/w/" + obj.weather[0].icon + ".png";
+        } else {
+            console.log("Delhi data not available");
         }
+    };
+    request.send();
+}
 
-        if(request.status>=200 && request.status < 400){
-            var temp=obj.main.temp;
-        }
-        else{
-            console.log("The city data are not present yet");
-        }
-        request.send();
+function loadJharkhand() {
+    console.log('Jharkhand function working');
+    let newlink = "https://api.openweathermap.org/data/2.5/weather?q=Jharkhand&appid=32977ceffadad0ca3ac83a080bfee99e";
+    let newrequest = new XMLHttpRequest();
+    newrequest.open('GET', newlink, true);
 
-        //downword part
-        newlink = "https://api.openweathermap.org/data/2.5/weather?q=Jharkhand&appid=32977ceffadad0ca3ac83a080bfee99e"
-        var newrequest = new XMLHttpRequest;
-        newrequest.open('GET',newlink,true);
-        newrequest.onload = function(){
-            var newobj = JSON.parse(this.response);
-            console.log(newobj);
-            document.getElementById('n-weather').innerHTML=newobj.weather[0].description;
+    newrequest.onload = function() {
+        if (newrequest.status >= 200 && newrequest.status < 400) {
+            let newobj = JSON.parse(this.response);
+            document.getElementById('n-weather').innerHTML = newobj.weather[0].description;
             document.getElementById('n-location').innerHTML = newobj.name;
             document.getElementById('n-temp').innerHTML = Math.round(newobj.main.temp - 273.15);
-            document.getElementById('n-icon').src = "https://openweathermap.org/img/w/" + newobj.weather[0].icon + ".png"; 
+            document.getElementById('n-icon').src = "https://openweathermap.org/img/w/" + newobj.weather[0].icon + ".png";
+        } else {
+            console.log("Jharkhand data not available");
         }
+    };
+    newrequest.send();
+}
 
-        if(newrequest.status>=200 && newrequest.status < 400){
-            var newtemp=newobj.main.temp;
-        }
-        else{
-            console.log("The city data are not present yet");
-        }
-        newrequest.send();
-        
+
+document.getElementById('refresh').addEventListener('click', loadDelhi);
+document.getElementById('n-refresh').addEventListener('click', loadJharkhand);
+
+
+window.onload = function() {
+    loadDelhi();
+    loadJharkhand();
+}
