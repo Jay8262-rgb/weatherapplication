0187902e33589016887d8e8498f004ca13fe1ae9
Jay8262-rgb <keshri425@gmail.com>
2025-09-08 22:12:01 +0530

    full api model

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// upper part
 function loadDelhi() {
-    console.log('Delhi function working');
+    console.log('Delhi function is working');
+
     var link = "https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=32977ceffadad0ca3ac83a080bfee99e";
     var request = new XMLHttpRequest();
     request.open('GET', link, true);
 
-    request.onload = function() {
+    request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             let obj = JSON.parse(this.response);
+
             document.getElementById('weather').innerHTML = obj.weather[0].description;
             document.getElementById('location').innerHTML = obj.name;
             document.getElementById('temp').innerHTML = Math.round(obj.main.temp - 273.15);
@@ -15,18 +18,22 @@ function loadDelhi() {
             console.log("Delhi data not available");
         }
     };
+
     request.send();
 }
 
+// downword part
 function loadJharkhand() {
-    console.log('Jharkhand function working');
+    console.log('Jharkhand function is working');
+
     let newlink = "https://api.openweathermap.org/data/2.5/weather?q=Jharkhand&appid=32977ceffadad0ca3ac83a080bfee99e";
     let newrequest = new XMLHttpRequest();
     newrequest.open('GET', newlink, true);
 
-    newrequest.onload = function() {
+    newrequest.onload = function () {
         if (newrequest.status >= 200 && newrequest.status < 400) {
             let newobj = JSON.parse(this.response);
+
             document.getElementById('n-weather').innerHTML = newobj.weather[0].description;
             document.getElementById('n-location').innerHTML = newobj.name;
             document.getElementById('n-temp').innerHTML = Math.round(newobj.main.temp - 273.15);
@@ -35,15 +42,22 @@ function loadJharkhand() {
             console.log("Jharkhand data not available");
         }
     };
+
     newrequest.send();
 }
 
 
-document.getElementById('refresh').addEventListener('click', loadDelhi);
-document.getElementById('n-refresh').addEventListener('click', loadJharkhand);
-
-
-window.onload = function() {
+window.onload = function () {
     loadDelhi();
     loadJharkhand();
-}
+};
+
+document.getElementById('n-refresh').addEventListener('click', function (e) {
+    e.preventDefault();   
+    loadJharkhand();      
+});
+
+document.getElementById('refresh').addEventListener('click', function (e) {
+    e.preventDefault();   
+    loadDelhi();   
+});   
