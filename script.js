bcb999561b00d2ef63811f4680f8d2109d6447e2
Jay8262-rgb <keshri425@gmail.com>
2025-09-11 22:26:33 +0530

    update 1.0

--- script.js
+++ script.js
@@ -23,39 +23,36 @@ function loadDelhi() {
 }
 
 // downword part
-function loadJharkhand() {
-    console.log('Jharkhand function is working');
+// function loadJharkhand() {
+//     console.log('Jharkhand function is working');
 
-    let newlink = "https://api.openweathermap.org/data/2.5/weather?q=Jharkhand&appid=32977ceffadad0ca3ac83a080bfee99e";
-    let newrequest = new XMLHttpRequest();
-    newrequest.open('GET', newlink, true);
+//     let newlink = "https://api.openweathermap.org/data/2.5/weather?q=Jharkhand&appid=32977ceffadad0ca3ac83a080bfee99e";
+//     let newrequest = new XMLHttpRequest();
+//     newrequest.open('GET', newlink, true);
 
-    newrequest.onload = function () {
-        if (newrequest.status >= 200 && newrequest.status < 400) {
-            let newobj = JSON.parse(this.response);
+//     newrequest.onload = function () {
+//         if (newrequest.status >= 200 && newrequest.status < 400) {
+//             let newobj = JSON.parse(this.response);
 
-            document.getElementById('n-weather').innerHTML = newobj.weather[0].description;
-            document.getElementById('n-location').innerHTML = newobj.name;
-            document.getElementById('n-temp').innerHTML = Math.round(newobj.main.temp - 273.15);
-            document.getElementById('n-icon').src = "https://openweathermap.org/img/w/" + newobj.weather[0].icon + ".png";
-        } else {
-            console.log("Jharkhand data not available");
-        }
-    };
+//             document.getElementById('n-weather').innerHTML = newobj.weather[0].description;
+//             document.getElementById('n-location').innerHTML = newobj.name;
+//             document.getElementById('n-temp').innerHTML = Math.round(newobj.main.temp - 273.15);
+//             document.getElementById('n-icon').src = "https://openweathermap.org/img/w/" + newobj.weather[0].icon + ".png";
+//         } else {
+//             console.log("Jharkhand data not available");
+//         }
+//     };
 
-    newrequest.send();
-}
+//     newrequest.send();
+// }
 
 
 window.onload = function () {
     loadDelhi();
-    loadJharkhand();
+    // loadJharkhand();
 };
 
-document.getElementById('n-refresh').addEventListener('click', function (e) {
-    e.preventDefault();   
-    loadJharkhand();      
-});
+
 
 document.getElementById('refresh').addEventListener('click', function (e) {
     e.preventDefault();   
